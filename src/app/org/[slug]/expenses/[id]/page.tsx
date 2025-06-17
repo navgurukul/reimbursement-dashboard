051202a0ede88b13d0c6ca7a7574ad5943d6ad09
komalahire <komala@navgurukul.org>
2025-06-17 20:39:10 +0530

    Added Created  expenss name in table

--- src/app/org/[slug]/expenses/[id]/page.tsx
+++ src/app/org/[slug]/expenses/[id]/page.tsx
@@ -847,7 +847,7 @@ export default function ViewExpensePage() {
         <div className="lg:col-span-3">
           <Card>
             <CardHeader>
-              <CardTitle>Expense Details</CardTitle>
+              <CardTitle>Expense Details </CardTitle>
             </CardHeader>
             <CardContent className="space-y-4">
               <div className="grid grid-cols-2 gap-4">
